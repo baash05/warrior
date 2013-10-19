bee0fa7d57ca10342119a52f21fc402c2808dc3a
catherine <catherine.jones41@gmail.com>
2013-10-19 15:15:53 +1100

    Level 3

--- player.rb
+++ player.rb
@@ -1,7 +1,9 @@
 class Player
   def play_turn(warrior)
     space = warrior.feel
-    if space.empty?
+    if space.empty? and warrior.health <= 14
+      warrior.rest!
+    elsif space.empty?
       warrior.walk!
     else
       warrior.attack!
